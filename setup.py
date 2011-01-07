1fb79965f1021589b33f9937cdd95fc29a0abbab
David Sveningsson <ext@sidvind.com>
2011-01-07 23:49:58 +0100

    python3 compability

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 from setuptools import setup, find_packages, Extension
-import commands
+from subprocess import Popen, PIPE, STDOUT
 
-# http://code.activestate.com/recipes/502261-python-distutils-pkg-config/
+# Based on http://code.activestate.com/recipes/502261-python-distutils-pkg-config/
+# but replaced commands.getoutput with subprocess and some other minor fixes.
+# All to get it working with python3
 def pkgconfig(*packages, **kw):
     flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
 
-    for token in commands.getoutput("pkg-config --libs --cflags %s" % ' '.join(packages)).split():
-        if flag_map.has_key(token[:2]):
+    # run pkg-config
+    args = ['pkg-config', '--libs', '--cflags'] + list(packages)
+    output = Popen(args, stdout=PIPE, stderr=STDOUT).communicate()[0]
+
+    # parse output
+    for token in output.split():
+        if token[:2] in flag_map:
             kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
             
         else: # throw others to extra_link_args
             kw.setdefault('extra_link_args', []).append(token)
 
 
-    for k, v in kw.iteritems(): # remove duplicated
+    for k, v in kw.items(): # remove duplicated
         kw[k] = list(set(v))
 
     return kw
